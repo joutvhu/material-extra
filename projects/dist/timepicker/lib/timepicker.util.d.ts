ddb169b2048f9a19a6740b654083b74eaf466c47
Giao Ho <joutvhu@gmail.com>
2020-10-13 12:45:26 +0700

    Delete dist

--- projects/dist/timepicker/lib/timepicker.util.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class TimepickerUtil {
-    static isEqualsDeep(value1: any, value2: any): boolean;
-    static isRealNumber(value: any): boolean;
-    static toString(value: any): string;
-}
-//# sourceMappingURL=timepicker.util.d.ts.map
--- projects/dist/timepicker/material-extra-timepicker.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="@material-extra/timepicker" />
-export * from './public-api';
-//# sourceMappingURL=material-extra-timepicker.d.ts.map
