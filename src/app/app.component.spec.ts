87bbacb5c50bf6dc6b385e6ac3653a004625a502
Giao Ho <joutvhu@gmail.com>
2020-10-13 08:26:54 +0700

    Change library name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have as title 'material-extend'`, () => {
+    it(`should have as title 'material-extra'`, () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
-        expect(app.title).toEqual('material-extend');
+        expect(app.title).toEqual('material-extra');
     });
 
     it('should render title', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement;
-        expect(compiled.querySelector('.content span').textContent).toContain('material-extend app is running!');
+        expect(compiled.querySelector('.content span').textContent).toContain('material-extra app is running!');
     });
 });
